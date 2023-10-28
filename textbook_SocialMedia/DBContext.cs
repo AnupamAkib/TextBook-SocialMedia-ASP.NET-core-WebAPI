243f6b71dcdcb7b2db6d75a0b80737374c7a16c5
AnupamAkib <mirakib25@gmail.com>
2023-10-29 00:56:54 +0600

    first commit

---
+++ textbook_SocialMedia/DBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using textbook_SocialMedia.Models;
+
+namespace textbook_SocialMedia
+{
+    public class DBContext : DbContext
+    {
+        public DBContext(DbContextOptions<DBContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ textbook_SocialMedia/Models/User.cs
@@ -0,0 +1,14 @@
+﻿namespace textbook_SocialMedia.Models
+{
+    public class User
+    {
+        public string UserID { get; set; }
+        public string UserName { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Gender { get; set; }
+        public string Password { get; set; }
+
+    }
+}
